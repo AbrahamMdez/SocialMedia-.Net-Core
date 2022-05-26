98cc7b2aa3e09bb1df4e7b9fa2e0294ad759d73d
Abraham <nencho_1@hotmail.com>
2022-05-26 10:25:19 +0200

    Create Post controller and Post entitites

---
+++ SocialMedia.Api/Controllers/PostController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SocialMedia.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class PostController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetPosts()
+        {
+            return Ok(null);
+        }
+    }
+}
